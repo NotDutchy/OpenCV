d4fd5869ce86cd86fafa7fcaec38eaa4687af8f6
Alcides <g.saarloos@student.avans.nl>
2023-05-21 14:41:47 +0200

    added 1.3

---
+++ OpenCV/Opdracht1_3.cpp
@@ -0,0 +1,25 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main() {
+	Mat img(512, 512, CV_8UC3, Scalar(255, 255, 255));
+
+	putText(img, "Computer vision ", Point(30, 100), FONT_HERSHEY_DUPLEX, 1.75, Scalar(255, 69, 255), 3);
+	rectangle(img, Point(20, 50), Point(492, 120), Scalar(0, 0, 0), 2);
+
+	circle(img, Point(256, 320), 100, Scalar(0, 0, 0), 6);
+	circle(img, Point(220, 300), 20, Scalar(0, 0, 0), 4);
+	circle(img, Point(292, 300), 20, Scalar(0, 0, 0), 4);
+	circle(img, Point(256, 360), 30, Scalar(0, 0, 0), 4);
+
+	line(img, Point(246, 290), Point(200, 270), Scalar(0, 0, 0), 3);
+	line(img, Point(266, 290), Point(312, 270), Scalar(0, 0, 0), 3);
+
+	imshow("Image", img);
+	waitKey(0);
+}
